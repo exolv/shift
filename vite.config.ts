b5dc48eeafbf0ffffe92ba3f3bad309989e43b7a
xsuve <georgebaba99@yahoo.com>
2023-01-24 17:43:20 +0200

    Services alias.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   resolve:{
     alias:{
       '@pages': path.resolve(__dirname, './src/pages'),
-      '@components': path.resolve(__dirname, './src/components')
+      '@components': path.resolve(__dirname, './src/components'),
+      '@services': path.resolve(__dirname, './src/services'),
     },
   },
 })
