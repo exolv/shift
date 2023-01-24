50a30136b359221823bc4f1c6e59901b2d9904a4
xsuve <georgebaba99@yahoo.com>
2023-01-24 12:06:35 +0200

    Added size prop to Logo component.

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import shiftLogo from '../../assets/shift-logo.svg';
 
+type LogoPropsSize = 'regular' | 'lg';
+
 interface LogoProps {
+  size?: LogoPropsSize;
   className?: string;
 };
 
+const LogoPropsSizeMap = {
+  'regular': 'w-[64px]',
+  'lg': 'w-[128px]'
+};
+
 const Logo: React.FC<LogoProps> = ({
+  size = 'regular',
   className = ''
 }) => {
   return (
-    <div className={`w-[64px] ${className}`}>
+    <div className={`${LogoPropsSizeMap[size]} ${className}`}>
       <img src={shiftLogo} alt='shift' />
     </div>
   );
